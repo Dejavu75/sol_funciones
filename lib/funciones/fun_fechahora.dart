65ff8e7935eed819ee618e7c5d3d489acd22c972
Dejavu75 <diego.zacur@dvsoft.com.ar>
2024-04-08 00:39:43 -0300

    export

--- lib/funciones/fun_fechahora.dart
+++ lib/funciones/fun_fechahora.dart
@@ -7,18 +7,18 @@ DateTime convertirTZ(fecha, tz) {
   }
   return tzc.TZDateTime.from(fecha, tz);
 }
-String fechaLocal(String fechaT, tz) {
-  final fecha = convertirTZ(DateTime.parse(fechaT), tz);
-  if (fecha.year == 1980) {
-    return "";
-  }
-  return "${fecha.day.toString().padLeft(2, '0')}/${fecha.month.toString().padLeft(2, '0')}/${fecha.year.toString()}";
-}
 
-String fhLocal(String fechaT, [tz]) {
-  final fecha = convertirTZ(DateTime.parse(fechaT), tz);
+String fechaCorta(DateTime fecha, [tz]) {
+  fecha = convertirTZ(fecha, tz);
   if (fecha.year == 1980) {
     return "";
   }
-  return "${fecha.day.toString().padLeft(2, '0')}/${fecha.month.toString().padLeft(2, '0')}/${fecha.year.toString()} ${fecha.hour.toString().padLeft(2, '0')}:${fecha.minute.toString().padLeft(2, '0')}";
+  return "${fecha.day.toString().padLeft(2, '0')}/${fecha.month.toString().padLeft(2, '0')}/${fecha.year.toString().substring(2)}";
+}
+String fechaHoraCorta(DateTime fecha, [tz]) {
+  fecha = convertirTZ(fecha, tz);
+  if (fecha.year == 1980) {
+    return "";
+  }
+  return "${fecha.day.toString().padLeft(2, '0')}/${fecha.month.toString().padLeft(2, '0')}/${fecha.year.toString().substring(2)} ${fecha.hour.toString().padLeft(2, '0')}:${fecha.minute.toString().padLeft(2, '0')}";
 }
---
+++ lib/sol_funciones_api.dart
@@ -0,0 +1,5 @@
+
+library;
+
+export 'funciones/fun_archivos.dart';
+export 'funciones/fun_fechahora.dart';
